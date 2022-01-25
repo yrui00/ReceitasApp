96def74725c0dec560919db2c8c48943de1f1532
Yuri Salinas <yuri.salinas0@gmail.com>
2022-01-25 11:49:29 -0300

    limpeza coisas desnecessarios

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Server OK!');
 })
 
-app.listen(5000, () => {
-    console.log('5000');
+app.listen(port, () => {
+    console.log('http://localhost:'+port);
 })
+
