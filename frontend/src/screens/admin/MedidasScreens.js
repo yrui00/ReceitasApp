2094c72a5f2efae2ab5e5fdaaecc89de0d7fc315
Yuri Salinas <yuri.salinas0@gmail.com>
2022-04-07 18:25:34 -0300

    up

--- frontend/src/screens/admin/MedidasScreens.js
+++ frontend/src/screens/admin/MedidasScreens.js
@@ -2,13 +2,15 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import NavAdmin from '../../components/navAdmin';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useParams } from 'react-router-dom';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
 
 export default function AdminMedidasScreen() {
     return <div className='cont'>
         <Routes>
-            <Route path=":id" element={<Add />} />
-            <Route path="/add" element={<Add />} />
+            <Route path=":id" element={<Edit />} />
+            <Route path="/add" element={<Add /> } />
             <Route index element={<Listar />} />
         </Routes>
     </div>;
@@ -32,12 +34,18 @@ function Listar() {
                 <div className='item'>
                     <div className='col prop1' style={styles.prop1} >1</div>
                     <div className='col prop2' style={styles.prop2} >nome</div>
-                    <div className='col prop3' style={styles.prop3} >bts</div>
+                    <div className='col prop3' style={styles.prop3} >
+                        <EditRoundedIcon />
+                        <DeleteForeverRoundedIcon />
+                    </div>
                 </div>
                 <div className='item'>
                     <div className='col prop1' style={styles.prop1} >1</div>
                     <div className='col prop2' style={styles.prop2} >nome</div>
-                    <div className='col prop3' style={styles.prop3} >bts</div>
+                    <div className='col prop3' style={styles.prop3} >
+                        <EditRoundedIcon />
+                        <DeleteForeverRoundedIcon />
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,7 +59,27 @@ function Add() {
             <form>
                 <div className='campos'>
                     <div className='campo'>
-                        <TextField label="Medida" style={{ width: '100%' }} />
+                        <TextField label="Medida" style={{ width: '100%' }} size="small" />
+                    </div>
+                    <div className='campo bt'>
+                        <Button variant="contained">Enviar</Button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+}
+
+function Edit(props) {
+    const {id} = useParams();
+    console.log(id )
+    return <div>
+        <NavAdmin titulo="Medidas" bts={['voltar']} />
+        <div className='formAdmin'>
+            <form>
+                <div className='campos'>
+                    <div className='campo'>
+                        <TextField label="Medida" style={{ width: '100%' }} size="small" defaultValue={id} />
                     </div>
                     <div className='campo bt'>
                         <Button variant="contained">Enviar</Button>
